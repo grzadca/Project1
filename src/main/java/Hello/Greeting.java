09b52ab83de2f62aaea18f45e727d6ad4bb31e5f
grzegorz.rzadca <Kamehame123>
2017-02-01 22:53:42 +0100

    First Test

---
+++ src/main/java/Hello/Greeting.java
@@ -0,0 +1,23 @@
+package Hello;
+
+/**
+ * Created by grzegorz.rzadca on 31-01-2017.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
